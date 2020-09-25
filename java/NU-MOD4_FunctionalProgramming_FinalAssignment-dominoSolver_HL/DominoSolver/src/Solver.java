7b774365d9ac81c3f66c3878ecc0a33e27e779fa
Huub Lievestro <huub.lievestro@nedap.com>
2020-09-25 16:24:41 +0200

    Java: add validation-methods and adapt solving methods from Haskell

--- java/NU-MOD4_FunctionalProgramming_FinalAssignment-dominoSolver_HL/DominoSolver/src/Solver.java
+++ java/NU-MOD4_FunctionalProgramming_FinalAssignment-dominoSolver_HL/DominoSolver/src/Solver.java
@@ -1,42 +1,86 @@
-import javax.swing.text.Position;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solver {
 
     /**
-     * List of bones used in the current solution (PM: one array per solution)
+     * Grid of pips, input to solver
      */
-    private Bone[][] usedBones;
+    private PipGrid input;
 
-    private BoneGrid[] solutions;
+//    /** // TODO at top or in method?
+//     * List of bones used in the current solution (PM: one array per solution)
+//     */
+//    private Bone[][] availableBones;
 
+    /**
+     * List of found solutions
+     */
+    List solutions; // TODO not array (fixed size) but list
 
+    public Solver(PipGrid input) {
+        this.input = input;
+        this.solutions = new ArrayList<BoneGrid>();
+    }
 
-    private Grid[] solve (Grid input ) {
-BoneGrid currentSolution;
-//        -- CheckOrientaties voor positie (x,y)
-//        -- Is ((x,y) , (x+1,y)) een valide mogelijkheid?
-//                --   Zoja: CheckOrientaties voor de volgende positie
-//        --   Zonee: = oplossing|iets doms
-//                -- Is ((x,y), (x,y+1)) een valide mogelijkheid?
-//                --   Zoja: CheckOrientaties voor de volgende positie
-//        --   Zonee:  = oplossing|iets doms
-//                -- !! PM: orientation can also be "reversed" horizontal/vertical, because not all bones are symmetric
-//                -- misschien met een variable bord die in recursie steeds kleiner wordt als posities gevuld? Dan ook dat probleem ondervangen
+    private List solve (PipGrid input ) {
+      BoneGrid currentBoard = new BoneGrid();
+      Bone[] availableBones = new BoneSet().returnBones();
 
+    /* do something and solve it */
         return solutions ;
+    }
+
+    private BoneGrid gotoNextPosition(/*..*/){
+
+    }
+
+    private BoneGrid allOrientations(/*..*/){
+
+    }
+
+    private BoneGrid checkAndPlace(/*..*/){
+        if (availableBones.length == 1) {
+            if (/* valid placement */) {
+                // do place
+                // add resulting BoneGrid to List
+            } else {
+                // do nothing because nonesense
+            }
+        } else {
+            // if valid: gotoNextPosition
+            // if invalid: do nothing because nonesense
+        }
     }
 
     private boolean checkOrientation() {
         return false;
     }
 
-    private boolean valid() {
-        return false;
+
+    // Validation of placing bone on positions
+    private boolean valid(BoneGrid boneGrid, Grid.Position position1, Grid.Position position2,
+                          Bone bone, Bone[] availableBones) {
+        return validOnBoard(boneGrid, position1) && validOnBoard(boneGrid, position2) &&
+                validFree(boneGrid, position1) && validFree(boneGrid, position2) &&
+                validPipMatch(position1, position2, bone) && validNotUsed(bone, availableBones);
     }
 
-    private boolean validOnBoard (BoneGrid grid, Grid.Position position){
+    private boolean validOnBoard (BoneGrid grid, Grid.Position position) {
        return grid.isOnBoard(position);
     }
 
+    private boolean validFree (BoneGrid grid, Grid.Position position) {
+        return grid.isFree(position);
+    }
 
+    private boolean validPipMatch (Grid.Position position1, Grid.Position position2, Bone bone) {
+        return input.getPip(position1) == bone.getPipsLeft() && input.getPip(position2) == bone.getPipsRight();
+    }
+
+    private boolean validNotUsed (Bone bone, Bone[] availableBones) {
+        return Arrays.asList(availableBones).contains(bone);
+        // TODO: use a List or stream?
+    }
 }
